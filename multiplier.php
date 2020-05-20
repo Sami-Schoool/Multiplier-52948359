6d00b8b5dda8061e226591c5f70b8dc0aef41eb9
Sami School <2077560@talnet.nl>
2020-05-20 23:37:48 +0200

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,15 @@
+<?php 
+
+
+ // Vul een getal in 
+    echo "Ik ga twee getallen met elkaar vermenigvuldigen" . "\n"; 
+    $x = readline("Vul een getal in: ");
+    $y = readline("Vul een getal in: ");
+    $totaal = "Uw resultaat is: " . $x * $y;
+
+// laat totaal zien
+    echo $totaal;
+
+
+
+?>
